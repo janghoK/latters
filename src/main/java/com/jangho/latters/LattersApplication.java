0548ea2d33bfee250d0f7afe69af71699f74f783
janghoK <rlawkdgh0617@gmail.com>
2024-08-28 21:32:41 +0900

    project setting

---
+++ src/main/java/com/jangho/latters/LattersApplication.java
@@ -0,0 +1,13 @@
+package com.jangho.latters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LattersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LattersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jangho/latters/LattersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jangho.latters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LattersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
