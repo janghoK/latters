60acb77766b1ffe5ac12bfe110a1163b632d45ca
janghoK <rlawkdgh0617@gmail.com>
2024-08-31 19:47:32 +0900

    feat: user 테스트코드 추가

---
+++ src/test/java/com/jangho/latters/user/domain/UserTest.java
@@ -0,0 +1,51 @@
+package com.jangho.latters.user.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    @Test
+    @DisplayName("회원가입 테스트")
+    void createUser() {
+
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 테스트")
+    void createUserFail() {
+
+    }
+
+    @Test
+    @DisplayName("회원정보 수정 테스트")
+    void updateUser() {
+
+    }
+
+    @Test
+    @DisplayName("회원정보 수정 실패 테스트")
+    void updateUserFail() {
+
+    }
+
+    @Test
+    @DisplayName("회원탈퇴 테스트")
+    void deleteUser() {
+
+    }
+
+    @Test
+    @DisplayName("인증코드 검증 성공 테스트")
+    void verifyVerificationCode() {
+
+    }
+
+    @Test
+    @DisplayName("인증코드 검증 실패 테스트")
+    void verifyVerificationCodeFail() {
+
+    }
+
+}
